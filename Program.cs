989d6a72450f7bc0a356c85888fe8f9d6547dced
Letícia <83790997+leticialiyu@users.noreply.github.com>
2021-05-11 19:19:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] valores = { 20, 30, 40, 50 };
+            {
+                Console.WriteLine(valores[1]);
+                Console.ReadLine();
+            }
+
+
+            /*
+
+            // exercicio de if/else
+
+
+            int num1 = 25;
+            Console.WriteLine(num1);
+
+
+
+
+            Console.WriteLine("Digite sua idade");
+
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade <= 15)
+            {
+                Console.WriteLine("Inferior a 15");
+            }
+            else
+            {
+                Console.WriteLine("Superior a 15");
+            }
+
+            Console.ReadLine();
+
+
+            //fim do exercicio de if/else
+
+            // início do exercicio de função e retorno
+
+
+
+
+
+            Console.WriteLine("Digite um número");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+
+            int resultado = (num1 * num2);
+
+            Console.WriteLine("O resultado é: " + resultado);
+
+
+            int funcsoma = Soma(35, 50, 75);
+            Console.WriteLine(funcsoma);
+            Console.ReadLine();
+
+            */
+        }
+
+
+        /*
+        static int Soma(int a, int b, int c)
+        {
+            int resultado = a + b + c;
+            return resultado;
+
+        }
+
+    
+        // fim do exercicio de função e retorno
+
+        */
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
