7222a6244026e9d09f98fdcb071913f3baa1a297
Letícia <83790997+leticialiyu@users.noreply.github.com>
2021-05-13 17:03:55 -0300

    Add files via upload

---
+++ CalculadoraS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace calculadora_de_soma
+{
+    class Calc
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um número");
+            var num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número");
+            var num2 = int.Parse(Console.ReadLine());
+
+            int soma = num1 + num2;
+            Console.WriteLine("A soma é: " + soma);
+
+
+            Console.ReadLine();
+        }
+    }
+}
