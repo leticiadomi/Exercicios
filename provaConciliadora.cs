dbfbf14f29ac4a185d7b0817b66854e177c554fe
Letícia Domingos <83790997+leticialiyu@users.noreply.github.com>
2021-07-25 17:27:18 -0300

    Add files via upload

---
+++ provaConciliadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public enum status { Aguardando=1 , execução , Finalizado, Cancelado}
+
+        struct Processo
+        {
+            public string nome;
+            public string hora;
+            public dynamic status;
+
+            public Processo(string nome, string hora, dynamic status)
+            {
+                this.nome = nome;
+                this.hora = hora;
+                this.status = status;
+            }
+            public void ExibirProcesso()
+            {
+                Console.WriteLine($"Nome: {this.nome}");
+                Console.WriteLine($"Hora: {this.hora}");
+                Console.WriteLine($"Status: {this.status}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Processo limpar = new Processo("Limpar a Sala", "14:40", status.Aguardando);
+            Processo cachorro = new Processo("Dar comida para o cachorro", "09:00", status.Finalizado);
+
+            limpar.ExibirProcesso();
+            Console.WriteLine("--------------------------------------------------------------------");
+            cachorro.ExibirProcesso();
+            
+        }
+
+       
+    }
+}
